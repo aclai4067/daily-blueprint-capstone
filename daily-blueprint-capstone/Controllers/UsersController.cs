88a5ac7b369c520e938f5c227fa17ec1e872848e
Ashley Claiborne <languageisaliquid@gmail.com>
2020-07-07 21:55:26 -0500

    solution setup

---
+++ daily-blueprint-capstone/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using daily_blueprint_capstone.DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace daily_blueprint_capstone.Controllers
+{
+    [Route("api/employees")]
+    [ApiController]
+
+    public class EmployeesController : ControllerBase
+    {
+        EmployeesRepo _repository;
+
+        public EmployeesController(EmployeesRepo repository)
+        {
+            _repository= repository;
+        }
+    }
+}
---
+++ daily-blueprint-capstone/DataAccessLayer/ToDosRepo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace daily_blueprint_capstone.DataAccessLayer
+{
+    public class ToDosRepo
+    {
+        string ConnectionString;
+        public ToDosRepo(IConfiguration config)
+        {
+            ConnectionString = config.GetConnectionString("DailyBlueprint");
+        }
+    }
+}
