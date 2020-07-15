750a1b980f6afa6f01379eef40c36a826f6ee17c
Ashley Claiborne <languageisaliquid@gmail.com>
2020-07-14 20:34:44 -0500

    react setup

---
+++ daily-blueprint.ui/src/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-outline-light">Test Button</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ daily-blueprint.ui/src/components/shared/MyNav/MyNav.js
@@ -0,0 +1,26 @@
+import './MyNav.scss';
+import React from 'react';
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  Nav,
+  NavItem,
+  NavbarToggler,
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import PropTypes from 'prop-types';
+
+class MyNav extends React.Component {
+  render() {
+    return (
+      <div className='MyNav'>
+
+      </div>
+    );
+  }
+}
+
+export default MyNav;
