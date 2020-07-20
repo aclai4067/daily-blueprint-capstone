85e63eaf0141e064147fb53a14e32f5715d77a9b
Ashley Claiborne <languageisaliquid@gmail.com>
2020-07-19 22:58:23 -0500

    data pulling into team component

--- daily-blueprint.ui/src/components/pages/Team/Team.js
+++ daily-blueprint.ui/src/components/pages/Team/Team.js
@@ -1,7 +1,37 @@
 import './Team.scss';
 import React from 'react';
+import teamsData from '../../../helpers/data/teamsData';
+import UserShape from '../../../helpers/propz/UserShape';
+import toDoData from '../../../helpers/data/toDoData';
 
 class Team extends React.Component {
+  state = {
+    teams: [],
+    teamToDisplay: {},
+    teamPriorities: [],
+  }
+
+  static propTypes = {
+    user: UserShape.userShape,
+  }
+
+  componentDidMount() {
+    const { user } = this.props;
+    teamsData.getTeamsByUserId(user.id)
+      .then((userTeams) => {
+        const teamsArr = userTeams.data;
+        const primary = teamsArr.find((t) => t.isPrimary === true);
+        this.setState({ teams: teamsArr, teamToDisplay: primary });
+        this.getPrioritiesForCurrentTeam(primary.teamId);
+      }).catch((errorFromTeamsComponentDidMount) => console.error(errorFromTeamsComponentDidMount));
+  }
+
+  getPrioritiesForCurrentTeam = (teamId) => {
+    toDoData.getTeamPriorities(teamId)
+      .then((priorities) => this.setState({ teamPriorities: priorities.data }))
+      .catch((errorFromGetPrioritiesForCurrentTeam) => console.error(errorFromGetPrioritiesForCurrentTeam));
+  };
+
   render() {
     return (
       <div className='Team'>
