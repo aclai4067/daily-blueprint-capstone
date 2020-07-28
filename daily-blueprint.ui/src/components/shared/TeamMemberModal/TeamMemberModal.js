94537a88f8cfea57265ddfbdd67c9d841c0970da
Ashley Claiborne <languageisaliquid@gmail.com>
2020-07-28 01:00:33 -0500

    modal refresh on change working

--- daily-blueprint.ui/src/components/shared/TeamMemberModal/TeamMemberModal.js
+++ daily-blueprint.ui/src/components/shared/TeamMemberModal/TeamMemberModal.js
@@ -23,10 +23,11 @@ class TeamMemberModal extends React.Component {
     teamModalIsOpen: PropTypes.bool,
     toggleTeamModal: PropTypes.func,
     userId: PropTypes.number,
+    orgId: PropTypes.number,
   }
 
   componentDidUpdate(prevProps) {
-    if (this.props.teamModalIsOpen && this.props.teamModalIsOpen !== prevProps.teamModalIsOpen) {
+    if (this.props.teamModalIsOpen && (this.props.teamModalIsOpen !== prevProps.teamModalIsOpen || this.props.teamPriorities !== prevProps.teamPriorities)) {
       const { orgId } = this.props;
       this.getAllUsers(orgId);
     }
@@ -54,7 +55,7 @@ class TeamMemberModal extends React.Component {
   addTeamMate = (e) => {
     e.preventDefault();
     const { checkTeamLead, userToAdd } = this.state;
-    const { team, orgId } = this.props;
+    const { team, userId, getAllTeamData } = this.props;
     const teamMate = {
       userId: Number(userToAdd),
       teamId: team.teamId,
@@ -62,28 +63,30 @@ class TeamMemberModal extends React.Component {
       isPrimary: false,
     };
     teamsData.addTeamMember(teamMate)
-      .then(() => this.getAllUsers(orgId))
-      .catch((errorFromAddTeammate) => console.error(errorFromAddTeammate));
+      .then(() => {
+        getAllTeamData(userId);
+        this.setState({ userToAdd: '', checkTeamLead: false });
+      }).catch((errorFromAddTeammate) => console.error(errorFromAddTeammate));
   }
 
   deleteTeamMate = (e) => {
     e.preventDefault();
-    const { team, orgId } = this.props;
+    const { team, getAllTeamData, userId } = this.props;
     const userToRemove = {
       userId: Number(e.target.value),
       teamId: team.teamId,
     };
-    console.log(userToRemove);
     teamsData.removeTeamMember(userToRemove)
-      .then(() => this.getAllUsers(orgId))
-      .catch((errorFromDeleteTeammate) => console.error(errorFromDeleteTeammate));
+      .then(() => {
+        getAllTeamData(userId);
+        this.setState({ userToAdd: '', checkTeamLead: false });
+      }).catch((errorFromDeleteTeammate) => console.error(errorFromDeleteTeammate));
   }
 
   dismissTeamModal = (e) => {
     e.preventDefault();
-    const { toggleTeamModal, getAllTeamData, userId } = this.props;
+    const { toggleTeamModal } = this.props;
     this.setState({ userToAdd: '', checkTeamLead: false });
-    getAllTeamData(userId);
     toggleTeamModal();
   }
 
@@ -98,21 +101,20 @@ class TeamMemberModal extends React.Component {
     this.setState({ userToAdd: userId });
   }
 
+  buildTeamList = () => this.state.teamMembers.map((t) => <div key={`teamMember-${t.id}`} className='col-sm-4 d-flex'><p>{`${t.firstName} ${t.lastName}`}</p>
+      <button className='btn close' value={t.id} onClick={this.deleteTeamMate}>X</button></div>);
+
+  buildStaffDropdown = () => this.state.unassignedUsers.map((u) => <option key={`staff-${u.id}`} value={u.id}>{`${u.firstName} ${u.lastName}`}</option>);
+
   render() {
     const {
       checkTeamLead,
-      teamMembers,
-      unassignedUsers,
       userToAdd,
     } = this.state;
     const {
       teamModalIsOpen,
       toggleTeamModal,
     } = this.props;
-
-    const buildTeamList = () => teamMembers.map((t) => <div key={`teamMember-${t.id}`} className='col-sm-4 d-flex'><p>{`${t.firstName} ${t.lastName}`}</p>
-      <button className='btn close' value={t.id} onClick={this.deleteTeamMate}>X</button></div>);
-    const buildStaffDropdown = () => unassignedUsers.map((u) => <option key={`staff-${u.id}`} value={u.id}>{`${u.firstName} ${u.lastName}`}</option>);
 
     return (
       <div className='TeamMemberModal'>
@@ -122,18 +124,18 @@ class TeamMemberModal extends React.Component {
               <form className='unassignedUsers'>
                 <select className='form-control' id='userInput' value={userToAdd} onChange={this.userToAddChange}>
                   <option value='' disabled defaultValue>Select a Team Member</option>
-                  {buildStaffDropdown()}
+                  {this.buildStaffDropdown()}
                 </select>
                 <div>
                   <div>
-                    <input type='checkbox' className='teamLeadCheckbox' data-val='true' id='teamLeadCheck' value={checkTeamLead} onChange={this.teamLeadChange} />
+                    <input type='checkbox' className='teamLeadCheckbox' data-val='true' id='teamLeadCheck' checked={checkTeamLead} onChange={this.teamLeadChange} />
                     <label htmlFor='teamLeadCheck'>Make User Team Lead</label>
                   </div>
                   <button className='btn btn-secondary' onClick={this.addTeamMate}>Add</button>
                 </div>
               </form>
               <div className='teamMemembers d-flex flex-wrap'>
-                {buildTeamList()}
+                {this.buildTeamList()}
               </div>
               <Button className='dismissModal' onClick={this.dismissTeamModal}>Done</Button>
             </div>
