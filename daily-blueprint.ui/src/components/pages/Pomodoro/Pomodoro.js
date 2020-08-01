2c188273e0631e3f352d98f5eb8ec69b894798a0
Ashley Claiborne <languageisaliquid@gmail.com>
2020-08-01 14:20:44 -0500

    timer animation working

--- daily-blueprint.ui/src/components/pages/Pomodoro/Pomodoro.js
+++ daily-blueprint.ui/src/components/pages/Pomodoro/Pomodoro.js
@@ -10,12 +10,15 @@ import {
   faCheckCircle,
 } from '@fortawesome/free-regular-svg-icons';
 import chime from './Computer_Magic.mp3';
+import workChime from './watch_alarm.mp3';
 import pomodoroData from '../../../helpers/data/pomodoroData';
 
 class Pomodoro extends React.Component {
   state = {
     timerActive: false,
     remainingTime: 1500,
+    elaspsedDasharray: 283,
+    totalTime: 1500,
     displayTime: '25 : 00',
     session: 'work',
     workSessionCount: 0,
@@ -39,6 +42,7 @@ class Pomodoro extends React.Component {
         this.setState({
           isCustom: true,
           remainingTime: workSeconds,
+          totalTime: workSeconds,
           displayTime: `${settings.workMinutes} : 00`,
           workMinutes: settings.workMinutes,
           shortBreakMinutes: settings.shortBreakMinutes,
@@ -70,30 +74,56 @@ class Pomodoro extends React.Component {
       time -= 1;
       this.setState({ remainingTime: time });
       minutes = Math.floor(time / 60);
-      seconds = time - (minutes * 60);
+      seconds = time % 60;
       minutes = minutes < 10 ? `0${minutes}` : minutes;
       seconds = seconds < 10 ? `0${seconds}` : seconds;
       this.setState({ displayTime: `${minutes} : ${seconds}` });
+      this.setTimerCircle();
       if (time === 0) {
-        this.playEndOfSessionAlert();
+        this.setState({ elaspsedDasharray: 283 });
         if (session === 'work') { this.setState({ workSessionCount: this.state.workSessionCount + 1 }); }
         const { workSessionCount } = this.state;
         if (session === 'work' && workSessionCount % sessionsUntilLongBreak === 0) {
-          this.setState({ session: 'long', remainingTime: longBreakMinutes * 60, displayTime: `${longBreakMinutes} : 00` });
+          this.setState({
+            session: 'long',
+            remainingTime: longBreakMinutes * 60,
+            totalTime: longBreakMinutes * 60,
+            displayTime: `${longBreakMinutes} : 00`,
+          });
+          this.playEndOfWorkSessionAlert();
           // alert('Take a long break!');
         } else if (session === 'work' && workSessionCount % sessionsUntilLongBreak !== 0) {
-          this.setState({ session: 'short', remainingTime: shortBreakMinutes * 60, displayTime: `${shortBreakMinutes} : 00` });
+          this.setState({
+            session: 'short',
+            remainingTime: shortBreakMinutes * 60,
+            totalTime: shortBreakMinutes * 60,
+            displayTime: `${shortBreakMinutes} : 00`,
+          });
+          this.playEndOfWorkSessionAlert();
           // alert('Take a short break!');
         } else if (workSessionCount === totalSessions) {
           this.stopTimer();
         } else if (session !== 'work' && workSessionCount < totalSessions) {
-          this.setState({ session: 'work', remainingTime: workMinutes * 60, displayTime: `${workMinutes} : 00` });
+          this.playEndOfBreakSessionAlert();
+          this.setState({
+            session: 'work',
+            remainingTime: workMinutes * 60,
+            totalTime: workMinutes * 60,
+            displayTime: `${workMinutes} : 00`,
+          });
           // alert('Time to Work!');
         }
         this.buildSessionTracker();
       }
     }, 1000);
   };
+
+  setTimerCircle = () => {
+    const { totalTime, remainingTime } = this.state;
+    const remainingFraction = remainingTime / totalTime;
+    const remainingDasharray = `${(remainingFraction * 283).toFixed(0)} 283`;
+    this.setState({ elaspsedDasharray: remainingDasharray });
+  }
 
   pauseTimer = (e) => {
     e.preventDefault();
@@ -108,14 +138,22 @@ class Pomodoro extends React.Component {
     this.setState({
       timerActive: false,
       remainingTime: workSeconds,
+      elaspsedDasharray: 283,
+      totalTime: workSeconds,
       displayTime: `${workMinutes} : 00`,
       workSessionCount: 0,
       session: 'work',
     });
   };
 
-  playEndOfSessionAlert = () => {
-    const sessionChime = document.getElementById('sessionAlertChime');
+  playEndOfWorkSessionAlert = () => {
+    const sessionChime = document.getElementById('startBreakAlertChime');
+    sessionChime.play();
+  }
+
+  playEndOfBreakSessionAlert = () => {
+    const sessionChime = document.getElementById('startWorkAlertChime');
+    sessionChime.currentTime = 8.5;
     sessionChime.play();
   }
 
@@ -203,9 +241,11 @@ class Pomodoro extends React.Component {
     const {
       timerActive,
       displayTime,
+      elaspsedDasharray,
       workMinutes,
       shortBreakMinutes,
       longBreakMinutes,
+      session,
       sessionsUntilLongBreak,
       totalSessions,
       editTimer,
@@ -214,7 +254,7 @@ class Pomodoro extends React.Component {
     return (
       <div className='Pomodoro d-flex flex-wrap'>
         <h1 className='col-12'>Pomodoro Timer</h1>
-        <div className='timerDetails col-4'>
+        <div className='timerDetails col-md-4'>
           <div>
             <h4>Work Sessions Before Long Break</h4>
             { editTimer ? <input type='number' min={1} value={sessionsUntilLongBreak} onChange={this.sessionsUntilLongBreakchange} /> : <p>{sessionsUntilLongBreak}</p> }
@@ -239,21 +279,42 @@ class Pomodoro extends React.Component {
             { editTimer ? <button onClick={this.saveTimerSettingsEvent}>Save</button> : <button onClick={this.setEditTimer}>Settings</button> }
           </div>
         </div>
-        <div className='timer col-8'>
+        <div className='timer col-md-8'>
           <div className='countdown'>
-            <div className='timerBtns d-flex'>
+            <div className='timerBtns d-flex justify-content-center'>
               { timerActive ? <button className='btn close timerControlBtn' onClick={this.pauseTimer}><FontAwesomeIcon className='m-2 timerControlIcon' icon={faPauseCircle} /></button>
                 : <button className='btn close timerControlBtn' onClick={this.runTimer}><FontAwesomeIcon className='m-2 timerControlIcon' icon={faPlayCircle} /></button> }
               <button className='btn close timerControlBtn' onClick={this.stopTimer}><FontAwesomeIcon className='m-2 timerControlIcon' icon={faStopCircle} /></button>
             </div>
-            <h3>Work Time Remaining</h3>
-            <p>{displayTime}</p>
+            { session === 'work' ? <h3>Work Time Remaining</h3> : <h3>Break Time Remaining</h3>}
+            <div className='timerDisplay'>
+              <svg className='timerBackground' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
+                <g class='timerCircle'>
+                  <circle className='timeElapsed' cx='50' cy='50' r='45'></circle>
+                  <path
+                  id='remainingTimeDisplay'
+                  strokeDasharray={elaspsedDasharray}
+                  class='timeRemaining'
+                  d='
+                    M 50, 50
+                    m -45, 0
+                    a 45,45 0 1,0 90,0
+                    a 45,45 0 1,0 -90,0
+                  '
+                  ></path>
+                </g>
+              </svg>
+              <span className='timerCountdown'>{displayTime}</span>
+            </div>
           </div>
           <div className='sessionTrackerContainer'>
             {this.buildSessionTracker()}
           </div>
-          <audio id='sessionAlertChime'>
+          <audio id='startBreakAlertChime'>
             <source src={chime}></source>
+          </audio>
+          <audio id='startWorkAlertChime'>
+            <source src={workChime}></source>
           </audio>
         </div>
       </div>
